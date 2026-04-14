5d3ac27cefb337bfb0c5ade207cfb411e5891ae1
mary paredes <marygiulianaparedespereira@gmail.com>
2026-04-14 17:13:32 -0300

    M

---
+++ SELECT FirstName ,LastName FROM employee.sql
@@ -0,0 +1,6 @@
+SELECT FirstName ,LastName FROM employees
+ORDER by LastName ASC,FirstName ASC
+SELECT * FROM tracks join albums on tracks.AlbumId= albums.AlbumId
+Order by Milliseconds DES
+SELECT UnitPrice, name from  tracks
+Order by  UnitPrice  ASC LIMIT 10
