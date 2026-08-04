e6887d921eaf7e9536c9ff1c56011cb86cad8e83
mary <marygiulianaparedespereira@gmail.com>
2026-08-04 16:57:36 -0300

    Ejersicio

--- trabajopracticoCsv.py
+++ trabajopracticoCsv.py
@@ -1,3 +1,4 @@
+
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,7 +13,7 @@ restar_condena=df_filtrado['duracion_condena_anios'].sub(df[])
 print ("COLUMNA CATEGORIA")
 print(f"Anios con condena ;{restar_condena}.\n")
 
-if Default_limite_condenas := (restar_condena <):
+if Default_limite_condenas := (restar_condena > 10):
     print ("La condena es alta ,esta lesionado")
     print("Requiere revision inmediata")
 elif restar_condena< 10:
@@ -21,3 +22,50 @@ else:
     print ("mercado estable,sin alertas por el momento")
     #grafico de barras usando toda df
 print ("\n [Generando GRAFICO de barras]")
+
+  #
+filtro_avanzado=df[df]
+df_filtrado=df[filtro_avanzado]
+sumo_filas=df_filtrado['..'].sum()
+print ("filas")
+print(f"filas;{sumo_filas}.\n")
+
+  #Filtrar por texto parcial
+filtro_avanzado=df['ultima_situacion_laboral_descripcion'].str.startswith("Desocupado/a", na=False)
+df_filtrado=df[filtro_avanzado]
+print(filtro_avanzado)
+
+  #Filtrar por coincidencia exacta
+iltro_avanzado=df['ultima_situacion_laboral_descripcion']
+df_filtrado=df[df['Trabajador/ra de tiempo completo'] == 'Trabajador/ra de tiempo completo']
+print(filtro_avanzado)
+
+#Selección de Columnas Clave
+filtro_avanzado=df['nivel_instruccion_descripcion'].head()
+df_filtrado=df[filtro_avanzado]
+
+
+#Selección de Columnas Clave ............
+filtro_avanzado=df['estado_civil_id'].head()
+df_filtrado=df[filtro_avanzado]
+
+  #Agrupación y Resumen
+filtro_avanzado=df['estado_civil_descripcion'].groupby()
+df_filtrado=df[filtro_avanzado]
+sumo_filas=df_filtrado['estado_civil_id'].sum()
+print ("")
+
+  #Estructura de Control Automatizada
+ 
+if limite_alto_de_separaciones := (sumo_filas > 10):
+    print ("¡te separaste muchas veses.")
+    print("Requiere revision inmediata")
+elif sumo_filas < 10:
+    print("Aviso:Estas soltero hace 10 años")
+else:
+    print ("Estas bien ,sin problemas por el momento")
+
+# Gráfico de Barras Comparativo
+
+
+
